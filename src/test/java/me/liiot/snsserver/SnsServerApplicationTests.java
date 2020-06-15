015197689c90ba5ad5af22c9fb0146656143b56d
cyj199637 <cyj199637@gmail.com>
2020-06-16 03:07:31 +0900

    [#1] sns 프로젝트 생성

---
+++ src/test/java/me/liiot/snsserver/SnsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.liiot.snsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
