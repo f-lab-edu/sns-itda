5fc72e78a6b23ade2c16f16065b084e4833cd3b2
cyj199637 <cyj199637@gmail.com>
2020-09-25 02:10:19 +0900

    [#13] 피드백 반영
    - ObjectMapper를 빈으로 등록

--- src/main/java/me/liiot/snsserver/resolver/CurrentUserArgumentResolver.java
+++ src/main/java/me/liiot/snsserver/resolver/CurrentUserArgumentResolver.java
@@ -1,19 +1,25 @@
 package me.liiot.snsserver.resolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import me.liiot.snsserver.annotation.CurrentUser;
 import me.liiot.snsserver.model.User;
 import me.liiot.snsserver.util.SessionKeys;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
+@Component
+@RequiredArgsConstructor
 public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final ObjectMapper mapper;
 
     @Override
     public boolean supportsParameter(MethodParameter methodParameter) {
@@ -21,15 +27,18 @@ public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolve
     }
 
     @Override
-    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
-        HttpServletRequest servletRequest = (HttpServletRequest) nativeWebRequest.getNativeRequest();
-        HttpSession httpSession = servletRequest.getSession();
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonStr = (String) nativeWebRequest.getAttribute(SessionKeys.USER, WebRequest.SCOPE_SESSION);
+            User user = mapper.readValue(jsonStr, User.class);
 
-        String jsonStr = (String)httpSession.getAttribute(SessionKeys.USER);
-        User user = mapper.readValue(jsonStr, User.class);
-
-        return user;
+            return user;
+        } catch (IllegalArgumentException e) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
