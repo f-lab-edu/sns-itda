2c744ae65315a7c0b1f5d16d1e8caa2aeaee08bd
cyj199637 <cyj199637@gmail.com>
2020-11-28 23:12:39 +0900

    [#45] 외부 설정 주입 테스트

--- src/main/java/me/liiot/snsserver/config/WebConfig.java
+++ src/main/java/me/liiot/snsserver/config/WebConfig.java
@@ -2,6 +2,7 @@ package me.liiot.snsserver.config;
 
 import lombok.RequiredArgsConstructor;
 import me.liiot.snsserver.resolver.CurrentUserArgumentResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,10 +13,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${test.print}")
+    String test;
+
     private final CurrentUserArgumentResolver currentUserArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        System.out.println(test);
         resolvers.add(currentUserArgumentResolver);
     }
 }
